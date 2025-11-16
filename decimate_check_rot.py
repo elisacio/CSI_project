c3d6cfe471d7cd8bf3f2e5cc8ac29947e244c404
galinierh <hgln@OrdiHelia.>
2025-11-16 20:14:46 +0100

    changement patch vertex

--- decimate_check_rot.py
+++ decimate_check_rot.py
@@ -14,12 +14,14 @@ from getBFT_triangles import getBFT
 from encoding import encoding, encode
 from compute_wn import compute_wn
 
-def save_as_obj(path, vertices, faces):
+def save_as_obj(path, model):
     with open(path, "w") as f:
-        for v in vertices:
-            f.write(f"v {v[0]} {v[1]} {v[2]}\n")
-        for face in faces:
-            f.write(f"f {face.a+1} {face.b+1} {face.c+1}\n")
+        for (vertex_index, v) in enumerate(model.vertices):
+            if vertex_index not in model.deleted_vertices:
+                f.write(f"v {v[0]} {v[1]} {v[2]}\n")
+        for (face_index, face) in enumerate(model.faces):
+            if face_index not in model.deleted_faces:
+                f.write(f"f {face.a+1} {face.b+1} {face.c+1}\n")
 
 class OutputDel(obja.Output):
     def delete_face(self, index, face):
@@ -50,25 +52,20 @@ class Decimater(obja.Model):
 
         print('Taille faces set initial: ', len(self.faces))
 
-        for i in range(5):
+        for i in range(10):
             # partie networkx de badr, on a les indices des sommets à supprimer
-            indices_a_supprimer = get_independent_set(self) # resultat de badr
+            indexes_to_delete = get_independent_set(self) # resultat de badr
             # Enlever les faces et ressortir les patchs
-            patchs, operation = patch_vertex.patch_vertex(self, indices_a_supprimer)
+            patchs, patches_colors, formated_faces = patch_vertex.patch_vertex(self, indexes_to_delete, operations)
             print('taille set deleted_faces: ', len(self.deleted_faces))
             print('taille deleted_vertices: ', len(self.deleted_vertices))
-            retriangulated_patches, patches_colors = zigzag_coloration.retriangulation(patchs)
-
-            # add new faces (retriangulated_patches) to self.faces
-            new_faces = utils.flatten(retriangulated_patches)
-            last_index = len(self.faces)
-            formated_faces = [obja.Face(f[0], f[1], f[2]) for f in new_faces]
-            self.faces.extend(formated_faces)
+            
+            print('Taille faces ajoutées: ', len(formated_faces))
             print('Taille faces set after adding new ones: ', len(self.faces))
+            print(f"Nombre d'opérations {i} :", len(operations))
 
             #construction du vecteur de 0 et de 1
             new_colors = utils.flatten(patches_colors)
-            #faces_etape_i = list(set(self.faces) - self.deleted_faces)
             faces_etape_i = [self.faces[face_ind] for face_ind in range(len(self.faces)) if face_ind not in self.deleted_faces]
             print(f"Nombre de faces étape {i} :", len(faces_etape_i))
             ordre_faces_etape_i = getBFT(faces_etape_i)
@@ -79,26 +76,18 @@ class Decimater(obja.Model):
                     vecteur += str(new_colors[index])
                 else :
                     vecteur += "0"
-            #print(vecteur)
             vecteurs_couleurs.append(vecteur)
 
             # Encodage 
-            w_n = compute_wn(self, patchs, indices_a_supprimer)
+            w_n = compute_wn(self, patchs, indexes_to_delete)
             w_n = [tuple(np.round(w * 1000).astype(int)) for w in w_n]
             w_n_encoded, coloration_encoded = encoding(w_n, vecteur)
 
             encoded_wns.append(w_n_encoded)
             encoded_colorations.append(coloration_encoded)
 
-            # add new faces to operations ('delete', face_index, face)
-            for i in range(len(formated_faces)):
-                operations.append(('del', last_index+i, formated_faces[i]))
-
-            # ajout operation in operations
-            operations = operations + operation
-
         # Enregistrement du modèle avec le plus bas niveau de détail
-        save_as_obj("model_low.obj", self.vertices, self.faces)
+        save_as_obj("model_low.obj", self)
 
         # Iterate through the vertex
         for (vertex_index, vertex) in enumerate(self.vertices):
@@ -147,9 +136,9 @@ def main():
     """
     np.seterr(invalid = 'raise')
     model = Decimater()
-    model.parse_file('example/suzanne.obj')
+    model.parse_file('example/bunny.obj')
 
-    with open('example/suzanne_prolonge.obja', 'w') as output:
+    with open('example/bunny_prolonge2.obja', 'w') as output:
         model.contract(output)
 
 
--- patch_vertex.py
+++ patch_vertex.py
@@ -7,6 +7,7 @@ import networkx as nx
 from check_rotation import check_rotation
 import unittest
 import utils
+import zigzag_coloration
 
 def couple_index_face(face, deleted_index):
     result = [face.a, face.b, face.c]
@@ -31,29 +32,46 @@ def verifier_ferme(couples):
     couples_bis = set(utils.flatten(couples))
     return len(couples_bis) == len(couples)
 
-def patch_vertex(model, indices_a_supprimer):
+def patch_vertex(model, indices_a_supprimer, operations):
     """
     Prend en entrée les indices des vertex à supprimer, prend la liste des opérations de décimations
     Renvoie la liste des patchs ( la liste des indices dans l'ordre tel que la normale soit dans le bon sens )
     """
     patchs = set()
-    operation = []
+    retriangulated_patches = []
+    patches_colors = []
     centroid_shape = utils.shape_centroid(model.vertices)
+    index_face = len(model.faces)
+    
     """
     for (vertex_index, vertex) in enumerate(model.vertices):
         if vertex_index in indices_a_supprimer :"""
     for vertex_index in indices_a_supprimer:
+
         faces_voisines = utils.face_from_vertex(model, vertex_index)
         consecutive_list = ordonnement(model.faces, faces_voisines, vertex_index)
+
         if len(consecutive_list) != 0:
-            # print(consecutive_list)
+            
             list_vertex_sorted = check_rotation(model.vertices, consecutive_list, centroid_shape)
             patchs.add(tuple(list_vertex_sorted))
-            operation = operation + [('face', face_ind, model.faces[face_ind]) for face_ind in faces_voisines]
+
+            new_triangles, colors = zigzag_coloration.draw_zigzag(tuple(list_vertex_sorted))
+            retriangulated_patches.append(new_triangles)
+            patches_colors.append(colors)
+            
+            last_index = len(model.faces)
+            formated_faces_patch = [obja.Face(f[0], f[1], f[2]) for f in new_triangles]
+            model.faces.extend(formated_faces_patch)
+
+            # build operations
+            for i in range(len(formated_faces_patch)):
+                operations.append(('del', last_index+i, formated_faces_patch[i]))
+            
+            operations.extend([('face', face_ind, model.faces[face_ind]) for face_ind in faces_voisines])
             model.deleted_faces.update({face_ind for face_ind in faces_voisines})
-            # remove faces_voisines from faces !!!!! à mettre dans une variable
-            operation.append(('vertex', vertex_index, model.vertices[vertex_index]))
+            
+            operations.append(('vertex', vertex_index, model.vertices[vertex_index]))
             model.deleted_vertices.add(vertex_index)
-            # remove vertex_index from vertices !!!!! à mettre dans une variable
-    # remove les faces et vertex des set vertices and faces
-    return patchs, operation#, deleted_faces, deleted_vertices
+    
+    return patchs, patches_colors, model.faces[index_face:-1]
